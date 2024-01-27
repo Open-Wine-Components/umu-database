89355e9b1b674da33f5e7814d6d9d0b2cc6916a6
GloriousEggroll <gloriouseggroll@gmail.com>
2024-01-27 02:09:44 -0700

    add simple create tables sql script

---
+++ create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE game (
+    id VARCHAR(255) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    acronym VARCHAR(255)
+);
+
+CREATE TABLE gamerelease (
+    ulgwl_id VARCHAR(255) REFERENCES game(id),
+    codename VARCHAR(255),
+    store VARCHAR(255),
+    notes TEXT
+);
