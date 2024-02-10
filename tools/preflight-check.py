716196ff06bed6a7d6011121e4bd2dcbe77c2058
Paweł Lidwin <lidwinpawel@gmail.com>
2024-02-10 21:11:20 +0100

    apply review comments

--- tools/preflight-check.py
+++ tools/preflight-check.py
@@ -23,16 +23,15 @@ def main():
             codename = row[2]
             ulwgl_id = row[3]
 
-            if not title or not store or not codename or not ulwgl_id:
+            if not (title or store or codename or ulwgl_id):
                 print("At least one of the required fields is missing, in row:", i)
                 exit(1)
 
             # Check if we have the same title for all entries with same ulwgl-id
-            if ulwgl_id in id_title_map:
-                # Import ignores the case when adding new entries
-                if id_title_map[ulwgl_id].lower() != title.lower():
-                    print("Different title for same ulwgl_id found", id_title_map[ulwgl_id], title, ulwgl_id)
-                    exit(1)
+            # Import ignores the case when adding new entries
+            if ulwgl_id in id_title_map and id_title_map[ulwgl_id].lower() != title.lower():
+                print("Different title for same ulwgl_id found", id_title_map[ulwgl_id], title, ulwgl_id)
+                exit(1)
             else:
                 id_title_map.update({ulwgl_id: title})
 
